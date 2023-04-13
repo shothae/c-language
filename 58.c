5f7f79d0f6240d28f94b114c747fab8f6411a784
채건 (Gcxgh) <aegun01@gmail.com>
2023-04-13 02:43:51 +0000

    연산자

---
+++ 58.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    printf("%d %d\n", 3==2, 3!=2);
+	printf("%d %d\n", 3>2, 3<=2);
+	printf("%d\n", (3>2)&&(3<=2));
+	printf("%d\n", (3>2)||(3<=2));
+	printf("%d\n", !(3>2));
+	
+	
+	
+	return 0;
+}
